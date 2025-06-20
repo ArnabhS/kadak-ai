943c17af09135394f2a77b0d0d2af64497343cb9
ArnabhS <sinhaarnabh888@gmail.com>
2025-06-20 23:17:44 +0530

    Add Civic Auth integration and update dependencies in configuration files

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+import { createCivicAuthPlugin } from "@civic/auth-web3/nextjs"
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
+const withCivicAuth = createCivicAuthPlugin({
+  clientId: "46910eb0-2c81-42ae-be0a-0431ca03ba70"
+});
 
-export default nextConfig;
+export default withCivicAuth(nextConfig)
+
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { CivicAuthProvider } from "@civic/auth-web3/nextjs";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -26,8 +27,10 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      > 
+      <CivicAuthProvider>
         {children}
+        </CivicAuthProvider>
       </body>
     </html>
   );

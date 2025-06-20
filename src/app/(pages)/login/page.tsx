460c8379cbba0e0ce0e2bfa393071c2cc2ce9d8b
ArnabhS <sinhaarnabh888@gmail.com>
2025-06-20 22:29:33 +0530

    initialisation and auth setup

---
+++ src/app/(pages)/login/page.tsx
@@ -0,0 +1,13 @@
+import { UserButton } from '@civic/auth/react'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+    <h1>My App</h1>
+      <UserButton />
+    </div>
+  )
+}
+
+export default Page
